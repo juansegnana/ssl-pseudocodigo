7aa2959920cdfb963b6565553d3af6d4ddf40ae3
Hilel <45838224+hilelmshalom@users.noreply.github.com>
2021-05-29 16:20:56 -0300

    TD y separadores
    Se añadieron los tokens para Tipos de Datos y el conjunto de llaves y corchetes

--- src/lexer.py
+++ src/lexer.py
@@ -20,8 +20,6 @@ tokens = [
     'PROCESO',
     'ESCRIBIR',
     'LEER',
-    'PARENTESIS_ABIERTO',
-    'PARENTESIS_CERRADO',
     'O',
     'Y',
     'SI',
@@ -63,7 +61,18 @@ tokens = [
     #comentarios abajo
     'COMENTARIO_ENCABEZADO',
     'COMENTARIO_VARIASLINEAS',
-    'COMENTARIO_LINEA'
+    'COMENTARIO_LINEA',
+    #tipos de llaves
+    'PARENTESIS_ABIERTO',
+    'PARENTESIS_CERRADO',
+    'LLAVE_ABIERTA',
+    'LLAVE_CERRADA',
+    'CORCHETE ABIERTO',
+    'CORCHETE CERRADO',
+    #tipos de datos
+    'TD_NUMERICO',
+    'TD_ALFANUMERICO',
+    'TD_LOGICO'
 ]
 
 # ply detecta variables que empiecen con 't_'
@@ -105,6 +114,12 @@ def t_FIN_ACCION(t): r'(fin_accion|FIN_ACCION)'; return t
 
 def t_AMBIENTE(t): r'(ambiente|AMBIENTE)'; return t 
 
+def t_TD_NUMERICO(t): r'(entero|numero|numerico|ENTERO|NUMERO|NUMERICO)'; return t 
+
+def t_TD_ALFANUMERICO(t): r'(cadena|alfanumerico|CADENA|ALFANUMERICO)'; return t 
+
+def t_TD_LOGICO(t): r'(logico|booleano|LOGICO|BOOLEANO)'; return t 
+
 def t_PROCESO(t): r'(proceso|PROCESO)'; return t 
 
 def t_ESCRIBIR(t): r'(escribir|ESCRIBIR)'; return t 
@@ -114,6 +129,14 @@ def t_LEER(t): r'(leer|LEER)'; return t
 def t_PARENTESIS_ABIERTO(t): r'\('; return t 
 
 def t_PARENTESIS_CERRADO(t): r'\)'; return t 
+
+def t_CORCHETE_ABIERTO(t): r'\['; return t 
+
+def t_CORCHETE_CERRADO(t): r'\]'; return t 
+
+def t_LLAVE_ABIERTA(t): r'\{'; return t 
+
+def t_LLAVE_CERRADA(t): r'\}'; return t 
 
 def t_ASIGNACION(t): r':='; return t 
 
