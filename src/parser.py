311528336f0aa91fda4452e70a2e271a2d1f2027
GonzaloZelinka <gonzalozelinka@gmail.com>
2021-06-08 10:41:14 -0300

    avance 1 arreglos prod.

--- src/parser.py
+++ src/parser.py
@@ -7,37 +7,64 @@ argParser = argparse.ArgumentParser(description='Procesa tokens de pseudocodigo.
 argParser.add_argument('-f', '-pathFile', nargs='?',type=str, help='especificar ruta de archivo de texto de entrada a analizar.')
 argsParser = argParser.parse_args()
 pathFile = argsParser.f
+# Corregir ambiguedad  --> http://www.dabeaz.com/ply/ply.html#ply_nn27 
+precedence = (
+    ('nonassoc', 'MENOR_QUE', 'MENOR_O_IGUAL_QUE', 'MAYOR_QUE', 'MAYOR_O_IGUAL_QUE'),  # Nonassociative operators -> NO PERMITE a < b < c
+    ('left', 'SUMA', 'RESTA'),
+    ('left', 'MULTIPLICACION', 'DIVISION', 'DIVISION_ENTERA', 'MODULO', 'POTENCIA'),
+    # ('right', 'UMINUS'),            # Unary minus operator
+)
 
+#MINUSCULA PRODUCCIONES
+#MAYUSCULAS TOKENS
 def p_sigma (p):
-    ''' sigma : COMENTARIO_ENCABEZADO ACCION IDENTIFICADOR ES ambiente FIN_ACCION
-            | ACCION IDENTIFICADOR ES ambiente FIN_ACCION '''
+    ''' sigma : ejecucion '''
     print("SIGMA")
     print("Termino")
-    
+
+def p_ejecucion (p):
+    '''ejecucion : COMENTARIO_ENCABEZADO nombre bloque FIN_ACCION
+                | nombre bloque FIN_ACCION 
+    '''
+    print('Prod. Ejecucion')
+
+def p_nombre(p): 
+    '''nombre: ACCION IDENTIFICADOR ES'''
+    print('Prod. Nombre accion')
+
 def p_comentario_vl_l (p):
     '''comentario_vl_l : COMENTARIO_VARIASLINEAS
-                    | COMENTARIO_LINEA'''
-    print('Prod Comanterio VL L')
+                    | COMENTARIO_LINEA
+    '''
+    print('Prod Comentario VL L')
 
+def p_bloque(p):
+    '''bloque : ambiente proceso'''
+    
 def p_ambiente (p):
-    '''ambiente : AMBIENTE bloque_ambiente proceso 
-                | AMBIENTE comentario_vl_l bloque_ambiente proceso'''
+    '''ambiente : AMBIENTE bloque_ambiente
+                | AMBIENTE comentario_vl_l bloque_ambiente
+    '''
     print('Prod Ambiente')
 
-def p_bloque_ambiente (p):
+def p_bloque_ambiente_sr (p):
     '''
-        bloque_ambiente : variable_comentario_vl_l
+        bloque_ambiente : variable comentario_vl_l
                     | constante comentario_vl_l
-                    | variable comentario_vl_l bloque ambiente
-                    | constante comentario_vl_l bloque_ambiente
                     | comentario_vl_l
-                    | comentario_vl_l bloque ambiente
-                    | variable bloque_ambiente
-                    | constante bloque_ambiente
                     | variable
                     | constante
     '''
-    print('Prod. Bloque ambiente')
+    print('Prod. Bloque ambiente_sr')
+
+def p_bloque_ambiente_cr (p):
+    ''' bloque_ambiente : variable comentario_vl_l bloque_ambiente
+                    | constante comentario_vl_l bloque_ambiente
+                    | comentario_vl_l bloque_ambiente
+                    | variable bloque_ambiente
+                    | constante bloque_ambiente 
+    '''
+    print('Prod. Bloque ambiente_cr')
 
 def p_variable (p):
     '''variable : IDENTIFICADOR DOS_PUNTOS ftd_amb'''
@@ -47,13 +74,6 @@ def p_constante (p):
     '''constante : IDENTIFICADOR IGUAL tipo_dato'''
     print('Prod. Constante')
 
-#def p_identificador (p): 
-#    '''identificador : IDENTIFICADOR'''
-#    print('Prod. Identificador')
-
-
-
-
 def p_ftd_amb (p): 
     '''ftd_amb : TD_ALFANUMERICO 
                  | TD_NUMERICO
@@ -61,12 +81,9 @@ def p_ftd_amb (p):
     print('Prod. Tipo dato')
 
 def p_tipo_dato (p):
-    '''tipo_dato : '''
-
-
-
-
-
+    '''tipo_dato : CADENA 
+                | NUMERICO
+    '''
 
 def p_proceso (p):
     '''
@@ -74,35 +91,67 @@ def p_proceso (p):
             | PROCESO conj_sentencia
     '''
     print('Prod. proceso')
-
 def p_conj_sentencia (p):
-    '''conj_sentencia : s_escribir conj_sentencia 
-                    | s_escribir comentario_vl_l conj_sentencia
-                    | s_leer conj_sentencia 
-                    | s_leer comentario_vl_l conj_sentencia 
-                    | s_si conj_sentencia 
-                    | s_si comentario_vl_l conj_sentencia 
-                    | s_ciclos conj_sentencia 
-                    | s_ciclos comentario_vl_l conj_sentencia 
-                    | sentencia conj_sentencia 
-                    | sentencia comentario_vl_l conj_sentencia 
-                    | comentario_vl_l conj_sentencia 
-                    | s_escribir comentario_vl_l 
-                    | s_leer comentario_vl_l 
-                    | s_si comentario_vl_l
-                    | s_ciclos comentario_vl_l 
-                    | sentencia comentario_vl_l 
-                    | s_escribir 
-                    | s_leer 
-                    | s_si 
-                    | s_ciclos 
-                    | sentencia 
-                    | comentario_vl_l'''
-    print('Prod. Conjunto de sentencias')
+    '''conj_sentencia: sentencias conj_sentencia
+                    | sentencia
+    '''
 
-def p_s_escribir (p):
-    '''s_escribir : ESCRIBIR PARENTESIS_ABIERTO salida_esc PARENTESIS_CERRADO'''
-    print('Prod. escribir')
+def p_sentencia_asig (p):
+        '''sentencia : IDENTIFICADOR ASIGNACION tipo_dato
+                | IDENTIFICADOR ASIGNACION op_aritmetica
+        '''
+
+
+def p_sentencia_leer (p):
+    '''sentencia : LEER PARENTESIS_ABIERTO IDENTIFICADOR PARENTESIS_CERRADO'''
+
+def p_sentencia_escribir (p):
+    '''sentencia : ESCRIBIR PARENTESIS_ABIERTO salida_esc PARENTESIS_CERRADO'''
+
+def p_sentencia_mientras (p):
+    '''sentencia : MIENTRAS PARENTESIS_ABIERTO conj_condiciones PARENTESIS_CERRADO HACER conj_sentencia FIN_MIENTRAS'''
+
+def p_sentencia_repetir (p):
+    '''sentencia : REPETIR conj_sentencia HASTA_QUE PARENTESIS_ABIERTO conj_condiciones PARENTESIS_CERRADO'''
+
+def p_sentencia_para (p):
+    ''' sentencia : PARA PARENTESIS_ABIERTO idt_para PARENTESIS_CERRADO HASTA id_tipodato COMA id_tipodato HACER conj_sentencia FIN_PARA
+            | PARA PARENTESIS_ABIERTO idt_para PARENTESIS_CERRADO HASTA id_tipodato COMA HACER conj_sentencia FIN_PARA'''
+
+#def p_conj_sentencia (p):
+#    '''conj_sentencia : s_escribir conj_sentencia 
+#                    | s_escribir comentario_vl_l conj_sentencia
+#                    | s_leer conj_sentencia 
+#                    | s_leer comentario_vl_l conj_sentencia 
+#                    | s_si conj_sentencia 
+#                    | s_si comentario_vl_l conj_sentencia 
+#                    | s_segun conj_sentencia
+#                    | s_segun comentario_vl_l conj_sentencia
+#                    | s_ciclos conj_sentencia 
+#                    | s_ciclos comentario_vl_l conj_sentencia 
+#                    | sentencia conj_sentencia 
+#                    | sentencia comentario_vl_l conj_sentencia 
+#                    | comentario_vl_l conj_sentencia 
+#                    | s_escribir comentario_vl_l 
+#                    | s_leer comentario_vl_l 
+#                    | s_si comentario_vl_l
+#                    | s_segun comentario_vl_l
+#                    | s_ciclos comentario_vl_l 
+#                    | sentencia comentario_vl_l 
+#                    | s_escribir 
+#                    | s_leer 
+#                    | s_si 
+#                    | s_segun
+#                    | s_ciclos 
+#                    | sentencia 
+#                    | comentario_vl_l
+#    '''
+#    print('Prod. Conjunto de sentencias')
+
+
+#def p_s_escribir (p):
+#    '''s_escribir : ESCRIBIR PARENTESIS_ABIERTO salida_esc PARENTESIS_CERRADO'''
+#    print('Prod. escribir')
     
 def p_salida_esc (p):
     '''salida_esc : IDENTIFICADOR 
@@ -111,20 +160,20 @@ def p_salida_esc (p):
                 | CADENA COMA salida_esc'''
     print('Prod. salida_esc')
     
-def p_s_leer (p):
-    '''s_leer : LEER PARENTESIS_ABIERTO entrada_leer PARENTESIS_CERRADO'''
-    print('Prod. leer')
-    
-def p_entrada_leer (p):
-    '''entrada_leer : IDENTIFICADOR'''
-    print('Prod. entrada_leer')
+#def p_s_leer (p):
+#    '''s_leer : LEER PARENTESIS_ABIERTO entrada_leer PARENTESIS_CERRADO'''
+#    print('Prod. leer')
+#    
+#def p_entrada_leer (p):
+#    '''entrada_leer : IDENTIFICADOR'''
+#    print('Prod. entrada_leer')
 
-def p_sentencia (p):
-    '''
-        sentencia : IDENTIFICADOR ASIGNACION tipo_dato
-                | IDENTIFICADOR ASIGNACION op_aritmetica
-    '''
-    print('Prod. sentencia')
+#def p_sentencia (p):
+#    '''
+#        sentencia : IDENTIFICADOR ASIGNACION tipo_dato
+#                | IDENTIFICADOR ASIGNACION op_aritmetica
+#    '''
+#    print('Prod. sentencia')
 
 def p_id_tipodato (p):
     '''
@@ -132,13 +181,6 @@ def p_id_tipodato (p):
                 | tipo_dato
     '''
     print('Prod. tipo dato')
-
-def p_conj_operaciones (p):
-    '''
-        conj_operaciones : op_aritmetica
-                        | relacionales
-    '''
-    print('Prod. conj. operaciones')
     
 def p_op_aritmetica (p):
     '''
@@ -177,9 +219,9 @@ def p_t_relacional (p):
     '''
     print('prod. t_relacional')
     
-def p_op_logico (p):
+def p_t_op_logico (p):
     '''
-        op_logico : O
+        t_op_logico : O
                 | Y
     '''
     print('Prod. op logico')
@@ -209,39 +251,39 @@ def p_expresion (p):
                 | id_tipodato t_op_aritmetico expresion'''
     print('Prod. expresion')
     
-def p_s_segun (p):
-    '''s_segun : SEGUN PARENTESIS_ABIERTO IDENTIFICADOR PARENTESIS_CERRADO HACER conj_cond_segun FIN_SEGUN'''
-def p_conj_cond_segun (p):
-    '''conj_cond_segun : t_relacional id_tipodato DOS_PUNTOS conj_sentencia 
-                    | t_relacional id_tipodato DOS_PUNTOS conj_sentencia conj_cond_segun 
-                    | OTRO DOS_PUNTOS conj_sentencia'''
-    print('Prod. conj. cond. segun')
+#def p_s_segun (p):
+#    '''s_segun : SEGUN PARENTESIS_ABIERTO IDENTIFICADOR PARENTESIS_CERRADO HACER #conj_cond_segun FIN_SEGUN'''
+#def p_conj_cond_segun (p):
+#    '''conj_cond_segun : t_relacional id_tipodato DOS_PUNTOS conj_sentencia 
+#                    | t_relacional id_tipodato DOS_PUNTOS conj_sentencia conj_cond_segun 
+#                    | OTRO DOS_PUNTOS conj_sentencia'''
+#    print('Prod. conj. cond. segun')
 
-def p_s_ciclo (p):
-    '''s_ciclo : c_para 
-            | c_mientras 
-            | c_repetir 
-            | conj_sentencia'''
-    print('Prod. de ciclos')
-
-
+#def p_s_ciclos (p):
+#    '''s_ciclos : c_para 
+#            | c_mientras 
+#            | c_repetir 
+#            | conj_sentencia'''
+#    print('Prod. de ciclos')
 
 
-def p_c_mientras (p):
-    '''
-        c_mientras : MIENTRAS PARENTESIS_ABIERTO conj_condiciones PARENTESIS_CERRADO HACER conj_sentencia FIN_MIENTRAS
-    '''
-    print('Prod. c_mientras')
+
+
+#def p_c_mientras (p):
+#    '''
+#        c_mientras : MIENTRAS PARENTESIS_ABIERTO conj_condiciones PARENTESIS_CERRADO HACER #conj_sentencia FIN_MIENTRAS
+#    '''
+#    print('Prod. c_mientras')
     
-def p_c_repetir (p):
-    '''c_repetir : REPETIR conj_sentencia HASTA_QUE PARENTESIS_ABIERTO conj_condiciones PARENTESIS_CERRADO'''
-    print('Prod. Repetir')
-def p_para (p):
-    '''
-        para : PARA PARENTESIS_ABIERTO idt_para PARENTESIS_CERRADO HASTA id_tipodato COMA id_tipodato HACER conj_sentencia FIN_PARA
-            | PARA PARENTESIS_ABIERTO idt_para PARENTESIS_CERRADO HASTA id_tipodato COMA HACER conj_sentencia FIN_PARA 
-    '''
-    print('Prod. Para')
+#def p_c_repetir (p):
+#    '''c_repetir : REPETIR conj_sentencia HASTA_QUE PARENTESIS_ABIERTO conj_condiciones #PARENTESIS_CERRADO'''
+#    print('Prod. Repetir')
+#def p_c_para (p):
+#    '''
+#        c_para : PARA PARENTESIS_ABIERTO idt_para PARENTESIS_CERRADO HASTA id_tipodato COMA #id_tipodato HACER conj_sentencia FIN_PARA
+#            | PARA PARENTESIS_ABIERTO idt_para PARENTESIS_CERRADO HASTA id_tipodato COMA HACER #conj_sentencia FIN_PARA 
+#    '''
+#    print('Prod. Para')
 def p_idt_para (p):
     '''idt_para : IDENTIFICADOR ASIGNACION id_tipodato 
             | IDENTIFICADOR
@@ -251,12 +293,12 @@ def p_idt_para (p):
 def p_error (p):
     print('Error parser', p)
 
-def p_empty(p):
-    '''
-        empty :
-    '''
-    # p[0] = None
-    print('Prod empty')
+#def p_empty(p):
+#    '''
+#        empty :
+#    '''
+#    # p[0] = None
+#    print('Prod empty')
         
 parser = yacc.yacc()
 print('Parser pseudocodigo.')
